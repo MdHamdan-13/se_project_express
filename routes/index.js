900d23b85959b4f2ff72fe193b07931f9ff0321f
Mohamed Hamdan <hamdan41d@gmail.com>
2025-01-31 19:17:32 -0600

    Updated Routes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ const { NOT_FOUND } = require("../utils/errors");
 
 const userRouter = require("./users");
 const clothingRouter = require("./clothingItems");
+const { login, createUser } = require("../controllers/users");
+
+router.post("/signin", login);
+router.post("/signup", createUser);
 
 router.use("/users", userRouter);
 router.use("/items", clothingRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const { getUsers, getUser, createUser } = require("../controllers/users");
 
-router.get("/", getUsers);
+// router.post("/", createUser);
+// router.get("/", getUsers);
 router.get("/:userId", getUser);
-router.post("/", createUser);
 
 module.exports = router;
+
+// START STEP 5
