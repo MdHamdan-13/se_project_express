cb6f67583ecd3c122d4f3d3ec833e33d3d57b0de
Mohamed Hamdan <hamdan41d@gmail.com>
2025-03-16 16:57:52 -0500

    Added validation and logging

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const errorHandler = require("./middlewares/error-handler");
 const cors = require("cors");
 const indexRouter = require("./routes/index");
+const { errors } = require("celebrate");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -17,7 +19,12 @@ mongoose
 app.use(express.json());
 app.use(cors());
 
+app.use(requestLogger);
 app.use("/", indexRouter);
+
+app.use(errorLogger);
+
+app.use(errors());
 app.use(errorHandler);
 
 app.listen(PORT, () => {
