984bc709f9217142b4d163279d64adb569450375
Mohamed Hamdan <hamdan41d@gmail.com>
2025-03-23 17:18:23 -0500

    Added Server Crash Test

--- app.js
+++ app.js
@@ -21,6 +21,13 @@ app.use(requestLogger);
 app.use(cors());
 app.use(express.json());
 
+// For testing purposes
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+
 app.use("/", indexRouter);
 
 app.use(errorLogger);
